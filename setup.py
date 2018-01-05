632c06a84f94311a175903ea3c1506c976d8f3b1
Konstantin Molchanov <moigagoo@live.com>
2018-01-05 12:10:27 +0400

    Bump version to 1.0.2.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     name='foliantcontrib.pandoc',
     description=SHORT_DESCRIPTION,
     long_description=LONG_DESCRIPTION,
-    version='1.0.1',
+    version='1.0.2',
     author='Konstantin Molchanov',
     author_email='moigagoo@live.com',
     url='https://github.com/foliant-docs/foliantcontrib',
