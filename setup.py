97f0a3ed3ccaece9e2849ef935c72d35162b98be
minuhin <minuhin@restream.rt.ru>
2021-07-21 15:09:31 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     install_requires=[
         'foliant>=1.0.8',
         'foliantcontrib.flatten>=1.0.2',
-        'foliantcontrib.meta>=1.3.2'
+        'foliantcontrib.meta>=1.3.2',
     ],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
